c36cb048726ac7a740451f5795c482329b2e0fc4
rthowel <rthowel@g.clemson.edu>
2020-09-29 19:51:54 -0400

    Attempting to connect STT to websockets to allow for one continuous connection. Currently does not work. Buffer_Queue does work but no data is returned to on_data in watsonCall

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from flask import Flask
 from flask_sockets import Sockets
-
+import watsonCall
 from server.services import *
+from Assistant import assistant
 
 import sys, traceback
+# import stt
+from queue import Queue
 
 app = Flask(__name__, template_folder='public/', static_folder='public/', static_url_path='')
-
 sockets: Sockets = Sockets(app)
+
+# buffer queue (main.py)
+BUFFER_MAX_ELEMENT = 20
+buffer_queue = Queue(maxsize=BUFFER_MAX_ELEMENT)
+
+stt_dict = watsonCall.watson_streaming_stt(buffer_queue, content_type="audio/l16;rate=16000;channels=1")
 
 # index
 @app.route('/')
 def hello_world():
-    return app.send_static_file('index.html')
+    return app.send_static_file('newIndex.html') #index.html is normal site
 
 
 @app.errorhandler(404)
@@ -26,12 +34,23 @@ def page_not_found(error):
 def requests_error(error):
     return app.send_static_file('500.html')
 
-
 # socket
 def stt(message):
     print("TODO: act upon this message")
     pass
 
+# @app.route('/initialize_stt')
+# def initialize_stt(audio_metadata):
+    # parse audio metadata
+
+    # initialize watson stt websocket (stt.py)
+    # stt_dict = stt.watson_streaming_stt(buffer_queue, content_type="audio/l16;rate=%i" % RATE)
+
+    pass
+
+    # Stop stt - I might need this later
+    # stt_dict["audio_source"].completed_recording()
+    # stt_dict["stream_thread"].join()
 
 @sockets.route('/api')
 def api(socket):
@@ -40,12 +59,24 @@ def api(socket):
             message = socket.receive()
             if isinstance(message, bytearray):
                 print("Got a bytearray from the client. Length:", len(message))
-                stt(message)
-                socket.send("Recieved bytearray. len(%i)" % len(message))
-                socket.send(message)
+                # stt(message)
+                # socket.send("Recieved bytearray. len(%i)" % len(message))
+                # socket.send(message)
+
+                buffer_queue.put(message)
+                x = 1
+
+                # transcript = stt(message)
+                # intent = assistant(transcript)
+                # socket.send(intent)
+                # print(intent)
             else:
                 print("Got this from the client:", message)
                 socket.send(message)
+
+                assistantmsg = assistant(message)
+                print("Got this from the api assistant:", assistantmsg)
+                socket.send(assistantmsg)
 
         except WebSocketError as e:
             print("WebSocketError:", e)
