f8a982d3fd5f57e70b7b16f64464749d82d313f2
rthowel <rthowel@g.clemson.edu>
2020-09-29 20:37:49 -0400

    Attempting to connect STT to websockets to allow for one continuous connection. Currently does not work. Buffer_Queue does work but no data is returned to on_data in watsonCall
    WatsonCall part of the attempt

--- watsonCall.py
+++ watsonCall.py
@@ -19,7 +19,7 @@ speech_to_text.set_service_url(dallasUrl)
 def sttCall(audio):
     speech_recognition_results = speech_to_text.recognize(
         audio=audio,
-        content_type='audio/l16;rate=22050;channels=1',
+        content_type='audio/l16;rate=16000;channels=1',
         # rate = 22050, # TODO: pass as argument
         # channels = 1, # TODO: pass as argument
         word_alternatives_threshold=0.3,
@@ -32,6 +32,44 @@ def sttCall(audio):
         transcript.append(text)
 
     return transcript
+
+
+# new stuff - speech recognition function
+from threading import Thread
+from ibm_watson.websocket import RecognizeCallback, AudioSource
+
+class MyRecognizeCallback(RecognizeCallback):
+    def __init__(self):
+        RecognizeCallback.__init__(self)
+
+    def on_data(self, data): # why is the transcript not showing up in on-data
+        print(json.dumps(data, indent=2))
+
+    def on_error(self, error):
+        print('Error received: {}'.format(error))
+
+    def on_inactivity_timeout(self, error):
+        print('Inactivity timeout: {}'.format(error))
+
+def watson_streaming_stt(buffer_queue, content_type):
+    audio_source = AudioSource(buffer_queue, True, True)
+    callback = MyRecognizeCallback()
+    stt_stream_thread = Thread(
+        target=speech_to_text.recognize_using_websocket,
+        kwargs=dict(
+            audio=audio_source,
+            content_type= content_type,
+            recognize_callback=callback,
+            model='en-US_BroadbandModel',
+            interim_results=False,
+            max_alternatives=1)
+    )
+    stt_stream_thread.start()
+
+    # return everything needed by main script
+    return {'audio_source': audio_source,
+            'stream_thread': stt_stream_thread}
+
 
 # return transcribed output
 # put auth in main script
