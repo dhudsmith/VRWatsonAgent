96c7ffc5dee57b6367b7830c3a972d89c4c0c332
Hudson <dhudsmith@gmail.com>
2021-05-14 10:15:24 -0400

    Updating to python 3.8

--- run_app.cmd
+++ run_app.cmd
@@ -1,1 +1,1 @@
-docker run -it --rm --env-file .env -v C:\Users\Hudson\PycharmProjects\VRWatsonAgent:/app/ -w /app/ -p 5000:5000 virtual-agent:latest python3.7 app/app.py
+docker run -it --rm --env-file .env -v C:\Users\Hudson\PycharmProjects\VRWatsonAgent:/app/ -w /app/ -p 5000:5000 virtual-agent:latest python3.8 app/app.py
